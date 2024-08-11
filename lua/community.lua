11e2a2c4beb1989822c55dd4a2749a1ed568c8a1
Emile Bangma <github@emilebangma.com>
2024-08-11 21:03:18 +0200

    Update community.lua

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -8,5 +8,11 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  -- https://docs.astronvim.com/recipes/colorscheme/
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- https://docs.astronvim.com/recipes/vscode/
+  -- { import = "astrocommunity.recipes.vscode" },
+  -- https://docs.astronvim.com/recipes/neovide/
+  { import = "astrocommunity.recipes.neovide" },
   -- import/override with your plugins folder
 }
