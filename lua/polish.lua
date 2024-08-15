f53115bcce0d599d6bbda691ee1df370907b81c8
saberzero1 <github@emilebangma.com>
2024-08-15 21:51:38 +0200

    Updated Neovide config to allow ctrl+x/c/v controls

--- lua/polish.lua
+++ lua/polish.lua
@@ -21,3 +21,18 @@
 -- vim.api.nvim_create_autocmd("BufEnter", {
 --   command = "set nornu nu",
 -- })
+
+if vim.g.neovide then
+  vim.keymap.set("n", "<C-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<C-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<C-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<C-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<C-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<C-v>", '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap("", "<C-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<C-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<C-v>", "<C-R>+", { noremap = true, silent = true })
